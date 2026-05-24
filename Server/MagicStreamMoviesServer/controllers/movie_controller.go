9bd2499412ca63e933202fe4cd4740fd6316127c
sabrrise <shodruzxoshimzoda@gmail.com>
2026-05-25 01:19:00 +0500

    Создал фунцию для добавления фильмов в БД с валидацией по структурным тегам

--- Server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -7,13 +7,18 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/shodruzxoshimzoda/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	models "github.com/shodruzxoshimzoda/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
+// Берём коллекцию фильмов (таблицу)
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
+
+//  Создаём структуру валидатора
 
 // Возвращает список фильмов
 func GetMovies() gin.HandlerFunc {
@@ -46,27 +51,60 @@ func GetMovies() gin.HandlerFunc {
 // Получение одного определённого фильма по его уникальному идентификатору
 func GetMovie() gin.HandlerFunc {
 	return func(c *gin.Context) {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second * 100)
-			defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
 
-			movieID := c.Param("imdb_id") // Берём идентификатор фильма из тело-запроса
+		movieID := c.Param("imdb_id") // Берём идентификатор фильма из тело-запроса
 
-			if movieID == "" {
-				c.JSON(http.StatusNotFound, gin.H{"error":"movie ID required"})
-				return 
-			}
+		if movieID == "" {
+			c.JSON(http.StatusNotFound, gin.H{"error": "movie ID required"})
+			return
+		}
 
-			var movie models.Movie
+		var movie models.Movie
 
-			// Находим фильм с таким идентификатором и декодируем его
-			err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
+		// Находим фильм с таким идентификатором и декодируем его
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
 
-			if err != nil {
-				c.JSON(http.StatusNotFound, gin.H{"error":"movie not found"})
-				return
-			}
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "movie not found"})
+			return
+		}
 
-			c.JSON(http.StatusOK, gin.H{"movie":movie})
+		c.JSON(http.StatusOK, gin.H{"movie": movie})
 
-			
-	}}
+	}
+}
+
+// Добавление фильмов
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+
+		err := c.ShouldBindJSON(&movie)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input", "details": err.Error()})
+			return
+		}
+
+		// Валидируем структуру по его структураным тегам ( "validate": "required")
+		if err = validate.Struct(movie); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "validation failed", "details": err.Error()})
+			return
+		}
+
+		// Добавляем фильм в нашу структуру
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to add movie", "details": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
+
+	}
+}

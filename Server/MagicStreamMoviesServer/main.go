89024371f54b663d3abede605480194e8f8fc92d
sabrrise <shodruzxoshimzoda@gmail.com>
2026-05-29 22:25:53 +0500

    Создал эндпоинт для логирования

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -18,6 +18,7 @@ func main() {
 	router.GET("/movie/:imdb_id", controller.GetMovie())
 	router.POST("/addmovie", controller.AddMovie())
 	router.POST("/register", controller.RegisterUser())
+	router.POST("/login",controller.LogginUser())
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to run server: ", err)
 	}

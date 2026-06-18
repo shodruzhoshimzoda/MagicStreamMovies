dcb2639c34651cbd48df66d3da626506e5a81657
sabrrise <shodruzxoshimzoda@gmail.com>
2026-06-18 23:43:57 +0500

    Зарегистрировал путь для получения жанра фильмов

--- Server/MagicStreamMoviesServer/routes/unprotected_routes.go
+++ Server/MagicStreamMoviesServer/routes/unprotected_routes.go
@@ -13,5 +13,7 @@ func SetupUnProtectedRoutes(router *gin.Engine, client  *mongo.Client) {
 	router.GET("/movies", controllers.GetMovies(client))
 	router.POST("/register", controllers.RegisterUser(client))
 	router.POST("/login", controllers.LogginUser(client))
+	router.GET("/genres", controllers.GetGenres(client))
+
 
 }
